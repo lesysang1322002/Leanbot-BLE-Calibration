002118c4d11541d5299d0a55686536a6ac10eb85
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2024-03-22 11:37:58 +0700

    update 22/03

--- script.js
+++ script.js
@@ -39,11 +39,12 @@ navigator.bluetooth.requestDevice({
     })
     .then(characteristic => {
         logstatus(dev.name);
+        checkMessageWithin5Seconds();
         document.getElementById("buttonText").innerText = "Rescan";
         checkconnected = true;
         gattCharacteristic = characteristic
         gattCharacteristic.addEventListener('characteristicvaluechanged', handleChangedValue);   
-        return gattCharacteristic.startNotifications()
+        return gattCharacteristic.startNotifications();
 })
 .catch(error => {
     if (error instanceof DOMException && error.name === 'NotFoundError' && error.message === 'User cancelled the requestDevice() chooser.') {
@@ -55,25 +56,27 @@ navigator.bluetooth.requestDevice({
     }
     });
 }}
+
 function disconnect()
 {
     logstatus("SCAN to connect");
     console.log("Disconnected from: " + dev.name);
     return dev.gatt.disconnect();
 }
+
 function onDisconnected(event) {
     const device = event.target;
     logstatus("SCAN to connect");
     document.getElementById("buttonText").innerText = "Scan";
     console.log(`Device ${device.name} is disconnected.`);
 }
-function send(data)
-{
+
+function send(data){
     console.log("You -> " + data + "\n");
     gattCharacteristic.writeValue(str2ab(data+"\n"));
 }
-function str2ab(str)
-{
+
+function str2ab(str){
     var buf = new ArrayBuffer(str.length);
     var bufView = new Uint8Array(buf);
     for (var i = 0, l = str.length; i < l; i++) {
@@ -81,6 +84,7 @@ function str2ab(str)
     }
     return buf;
 }
+
 function  logstatus(text){
     const navbarTitle = document.getElementById('navbarTitle');
     navbarTitle.textContent = text;
@@ -115,11 +119,16 @@ function Rescan(){
     toggleDisplayForElements(["Backbutton", "Next"], "block");
     Text_Area.value = " ";
     document.getElementById("Next").innerText = "Next";
+    checkmessage = false;
 }
+
 let string = "";
 let str = "";
 let angelLvalue = "";
 let angelRvalue = "";
+let checkmessage = false;
+let old00L,old00R,old90L,old90R;
+
 function handleChangedValue(event) {
     let data = event.target.value;
     let dataArray = new Uint8Array(data.buffer);
@@ -128,24 +137,40 @@ function handleChangedValue(event) {
     let n = valueString.length;
     if(valueString[n-1]=='\n'){
         string += valueString;
-        if(string[0]==='A'){
+        let stringcheck = string[0] + string[1] + string[2] + string[3] + string[4];
+        if (stringcheck === "GetCa" &&  !checkmessage) {
+            console.log("Previous message within 5 seconds.");
+            checkmessage = true;
+            clearTimeout(timeoutCheckMessage);// Hủy kết thúc sau 5 giây
             handleAction('Step1');
-            Step1();
+            let commaIndices = [];
+            for (let i = 0; i < string.length; i++) {
+                if (string[i] === ',') {
+                    commaIndices.push(i);
+                }
+            }
+            let leftBracketIndex = string.indexOf('(');
+            let rightBracketIndex = string.indexOf(')');
+            old00L = string.substring(leftBracketIndex + 1, commaIndices[0]);
+            old90L = string.substring(commaIndices[0] + 2, commaIndices[1]);
+            old00R = string.substring(commaIndices[1] + 2, commaIndices[2]);
+            old90R = string.substring(commaIndices[2] + 2, rightBracketIndex);
+            console.log(old00L + "," + old90L + "," + old00R + "," + old90R);
         }
-        else if(string[0]==='L'){
-            Step3();
-        }
-        else if(string[0]==='O'){
+        if(string[0]==='O'){
             Step1();
         }
         else if(string[0]==='C'){
             Step2();
         }
-        else if(string[0]==='W'){
+        else if(stringcheck === 'SetCa'){
+            Step3();
+        }
+        else if(string[0]==='T'){
             Step4();
         }
         else if(string[0]=='S'){
-            Text_Area.value = `Calibration Done`;
+            Text_Area.value = `TB1A + TB1B touched. Calibration settings saved. Calibration Done.`;
         }
         else if(string[0]==='d'){
             let LIndex = string.indexOf('L');
@@ -179,10 +204,6 @@ function handleAction(action) {
     }
 }
 
-let NextStepSave = false;
-
-let Done = false;
-
 let Step = 0;
 
 function Next() {
@@ -201,8 +222,9 @@ function Step1(){
     Step = 1;
     document.getElementById("Next").innerText = "Next";
     Text_Area.value = "Step 1/4: Adjust both gripper arms to proper 0° position (pointing down)";
-    Rvalue.value = "0";
-    Lvalue.value = "0";
+    Rvalue.value = old00R;
+    Lvalue.value = old00L;
+    sendLR();
     toggleDisplayForElements(["R0increment", "R0decrement", "L0increment", "L0decrement"], "block");
     toggleDisplayForElements(["R90increment", "R90decrement", "L90increment", "L90decrement"], "none");
 }
@@ -213,16 +235,15 @@ function Step2(){
     Text_Area.value = "Step 2/4: Adjust both gripper arms to proper 90° position (pointing horizontally)";
     toggleDisplayForElements(["R90increment", "R90decrement", "L90increment", "L90decrement"], "block");
     toggleDisplayForElements(["R0increment", "R0decrement", "L0increment", "L0decrement"], "none");
-    Rvalue.value = "90";
-    Lvalue.value = "90";
-    NextStepSave = true;
+    Rvalue.value = old90R;
+    Lvalue.value = old90L;
+    sendLR();
 }
 
 function Step3(){
     Step = 3;
     document.getElementById("Next").innerText = "Save";
     Text_Area.value = "Step 3/4: Observe gripper open and close correctly";
-    Done = true;
     toggleDisplayForElements(["R90increment", "R90decrement", "L90increment", "L90decrement"], "none");
 }
 
@@ -313,6 +334,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function sendLR(){
     send ('LR' + ' ' + Lvalue.value + ' ' + Rvalue.value);
+}
+
+let timeoutCheckMessage;
+
+function checkMessageWithin5Seconds() {
+    // Thiết lập hàm setTimeout để kết thúc sau 5 giây
+        timeoutCheckMessage = setTimeout(function() {
+        console.log("5 seconds timeout, message incorrect.");
+        let infoBox = document.getElementById("infopopup");
+        // Hiển thị info box
+        infoBox.style.display = "block";
+        document.addEventListener("click", function(event) {
+            if (!infoBox.contains(event.target)) {
+                infoBox.style.display = "none";
+            }
+        });
+    }, 5000);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
